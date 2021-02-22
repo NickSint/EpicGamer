78d39019f14b502f24892074755e5e30aa9fc356
Opilane TTHK <opilane@tthk.ee>
2021-02-22 15:44:23 +0200

    Add project files.

---
+++ FoodCreator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicGamer
+{
+    class FoodCreator
+    {
+		int mapWidht;
+		int mapHeight;
+		char sym;
+
+		Random random = new Random();
+
+		public FoodCreator(int mapWidth, int mapHeight, char sym,ConsoleColor color_)
+		{
+			this.mapWidht = mapWidth;
+			this.mapHeight = mapHeight;
+			this.sym = sym;
+			color_ = color_;
+		}
+
+		public Point CreateFood()
+		{
+			
+
+			int x = random.Next(2, mapWidht - 2);
+			int y = random.Next(2, mapHeight - 2);
+			return new Point(x, y, sym, ConsoleColor.Green);
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EpicGamer
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.SetWindowSize(80, 25);
+
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
+
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*', ConsoleColor.Red);
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
+
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$', ConsoleColor.Red);
+			Point food = foodCreator.CreateFood();
+			food.Draw();
+
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+
+
+		}
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("GG", xOffset + 1, yOffset++);
+			yOffset++;
+			
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+
+
+
+
+	}
+}
